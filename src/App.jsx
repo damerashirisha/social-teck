914f9fcb502113ed789c2b490d8603dea200db4e
damerashirisha <damerashirisha27@gmail.com>
2024-06-14 14:57:37 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/social-teck" element={<Home />} />
           <Route path="/about" element={<Aboutus />} />
           <Route path="/services" element={<OurServices />} />
           <Route path="/contact" element={<Test />} />
